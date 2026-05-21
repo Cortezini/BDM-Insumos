650071dfdcb65175015b94ab9e4060f16c49e1d2
Cortezini <matheusmoraescortezini@gmail.com>
2026-05-21 13:19:43 -0300

    Corrigido Calcts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     netlify(),
     react(),
