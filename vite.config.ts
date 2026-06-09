5839ff790fb18d77b254f798292a995e30546b30
Cortezini <matheusmoraescortezini@gmail.com>
2026-06-09 08:39:14 -0300

    Segunda  Versão

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
-    netlify(),
     react(),
   ],
-})
+});
